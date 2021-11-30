020406cfa2673321e953d3e623ca02ae43a8b55e
jidaojiuyou <2320541805@qq.com>
2021-11-30 16:11:10 +0800

    为了通用化，templateId改为templateCode

--- src/main/java/com/data/sms/util/TencentSmsUtils.java
+++ src/main/java/com/data/sms/util/TencentSmsUtils.java
@@ -26,16 +26,16 @@ public class TencentSmsUtils {
      *
      * @param phoneNumber   电话号码,例如: 18888888888
      * @param signName      签名名称,例如: 腾讯云
-     * @param templateId    短信模板ID,例如: 4****
+     * @param templateCode  短信模板ID,例如: 4****
      * @param templateParam 短信模板变量对应的实际值,例如:123456
      * @throws Exception 异常
      */
-    public SendSmsResponse sendSms(String phoneNumber, String signName, String templateId, String templateParam) throws Exception {
+    public SendSmsResponse sendSms(String phoneNumber, String signName, String templateCode, String templateParam) throws Exception {
         // 处理号码
         phoneNumber = "+86" + phoneNumber;
         String[] phoneNumberSet = {phoneNumber};
         // 生成请求,国内短信senderId填""
-        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, "", templateId, templateParam);
+        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, "", templateCode, templateParam);
         // 发送短信
         return tencentSmsClient.SendSms(req);
     }
@@ -45,17 +45,17 @@ public class TencentSmsUtils {
      *
      * @param phoneNumber   电话号码,例如: 18888888888
      * @param signName      签名名称,例如: 腾讯云
-     * @param templateId    短信模板ID,例如: 4****
+     * @param templateCode  短信模板ID,例如: 4****
      * @param templateParam 短信模板变量对应的实际值,例如:123456
      * @param listener      发送短信的响应监听器
      * @throws Exception 异常
      */
-    public void sendSms(String phoneNumber, String signName, String templateId, String templateParam, SendTencentSmsListener listener) throws Exception {
+    public void sendSms(String phoneNumber, String signName, String templateCode, String templateParam, SendTencentSmsListener listener) throws Exception {
         // 处理号码
         phoneNumber = "+86" + phoneNumber;
         String[] phoneNumberSet = {phoneNumber};
         // 生成请求,国内短信senderId填""
-        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, "", templateId, templateParam);
+        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, "", templateCode, templateParam);
         // 发送短信
         SendSmsResponse sendSmsResponse = tencentSmsClient.SendSms(req);
         // 回调响应
@@ -67,16 +67,16 @@ public class TencentSmsUtils {
      *
      * @param phoneNumber   电话号码,例如: +88618888888888
      * @param signName      签名名称,例如: 腾讯云
-     * @param templateId    短信模板ID,例如: 4****
+     * @param templateCode  短信模板ID,例如: 4****
      * @param templateParam 短信模板变量对应的实际值,例如:123456
      * @param senderId      国际/港澳台短信 SenderId
      * @throws Exception 异常
      */
-    public SendSmsResponse sendInternationalSms(String phoneNumber, String signName, String senderId, String templateId, String templateParam) throws Exception {
+    public SendSmsResponse sendInternationalSms(String phoneNumber, String signName, String senderId, String templateCode, String templateParam) throws Exception {
         // 处理号码
         String[] phoneNumberSet = {phoneNumber};
         // 生成请求
-        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, senderId, templateId, templateParam);
+        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, senderId, templateCode, templateParam);
         // 发送短信
         return tencentSmsClient.SendSms(req);
     }
@@ -86,16 +86,16 @@ public class TencentSmsUtils {
      *
      * @param phoneNumber   电话号码,例如: +88618888888888
      * @param signName      签名名称,例如: 腾讯云
-     * @param templateId    短信模板ID,例如: 4****
+     * @param templateCode    短信模板ID,例如: 4****
      * @param templateParam 短信模板变量对应的实际值,例如:123456
      * @param senderId      国际/港澳台短信 SenderId
      * @throws Exception 异常
      */
-    public void sendInternationalSms(String phoneNumber, String signName, String senderId, String templateId, String templateParam, SendTencentSmsListener listener) throws Exception {
+    public void sendInternationalSms(String phoneNumber, String signName, String senderId, String templateCode, String templateParam, SendTencentSmsListener listener) throws Exception {
         // 处理号码
         String[] phoneNumberSet = {phoneNumber};
         // 生成请求
-        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, senderId, templateId, templateParam);
+        SendSmsRequest req = generateSendSmsRequest(phoneNumberSet, signName, senderId, templateCode, templateParam);
         // 发送短信
         SendSmsResponse sendSmsResponse = tencentSmsClient.SendSms(req);
         // 回调响应
@@ -108,11 +108,11 @@ public class TencentSmsUtils {
      *
      * @param phoneNumberSet 电话号码集合
      * @param signName       签名
-     * @param templateId     模板
+     * @param templateCode     模板
      * @param templateParam  模板参数
      * @return 请求
      */
-    private SendSmsRequest generateSendSmsRequest(String[] phoneNumberSet, String signName, String senderId, String templateId, String templateParam) {
+    private SendSmsRequest generateSendSmsRequest(String[] phoneNumberSet, String signName, String senderId, String templateCode, String templateParam) {
         // 实例化一个请求对象
         SendSmsRequest req = new SendSmsRequest();
         // 设置号码
@@ -122,7 +122,7 @@ public class TencentSmsUtils {
         // 短信签名内容
         req.setSignName(signName);
         // 模板Id
-        req.setTemplateId(templateId);
+        req.setTemplateId(templateCode);
         // 处理模板参数
         String[] templateParamSet = {templateParam};
         // 设置模板参数
